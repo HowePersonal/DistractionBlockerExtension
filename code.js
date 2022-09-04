0a7c8d70695a336d360891436f392ddc4f693458
HowePersonal <howe2004new@gmail.com>
2022-09-03 20:57:01 -0700

    Added styling for blocked page's html replacement

--- code.js
+++ code.js
@@ -1,12 +1,45 @@
-let replaceHTML = (pageName) => {
-    return `<h1>Website Blocked</h1>`
+let replaceHTML = () => {
+    return `
+        <div class="blockedText">Website Blocked</div>
+    
+    `
+}
+
+let replaceSTYLE = () => {
+    return `<style>
+    html, body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width:100%;
+      height:100%;
+      margin:0;
+      padding:0;
+    }
+
+    body {
+      background: white;
+      overflow: hidden;
+      max-height: 700px;
+    }
+
+    .blockedText {
+      display: flex;
+      font-size: 30px;
+      color: black;
+      font-family: Arial;
+      font-weight: bold;
+    }
+
+    </style>`
 }
 
 
 fetch('http://localhost:9000').then(response => response.json())
   .then(data => {
     if (data["sites"].includes(window.location.hostname)) {
-        document.body.innerHTML = replaceHTML("Blocked Site")
+        document.head.innerHTML = replaceSTYLE()
+        document.body.innerHTML = replaceHTML()
     }
   })
 
